48ff8ac4120ab1ea530aafc49c217c43233964e7
evgeniy <ninjago58rus@gmail.com>
2022-08-31 16:51:59 +0300

    deploy,front

--- migrate-start.sh
+++ migrate-start.sh
@@ -3,6 +3,5 @@
 python ToDoManager/manage.py makemigrations
 python ToDoManager/manage.py migrate
 python ToDoManager/manage.py createadmin
-python ToDoManager/manage.py runserver 0.0.0.0:8000
-
+python ToDoManager/manage.py runserver
 exec "$@"
