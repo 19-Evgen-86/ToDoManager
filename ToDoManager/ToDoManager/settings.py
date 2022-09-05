dcd78cff9676fe364fdfbadc3dd2e08d690d0cef
evgeny <ninjago58rus@gmail.com>
2022-09-05 22:28:45 +0300

    deploy

--- ToDoManager/ToDoManager/settings.py
+++ ToDoManager/ToDoManager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG',False)
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,9 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.parent.joinpath("nginx",'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"))
+STATIC_ROOT = BASE_DIR.parent.joinpath("nginx", 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
