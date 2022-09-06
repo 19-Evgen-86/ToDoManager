2911d2b11a34b1cd0249cb5c1aa0dda7f7c531cc
evgeniy <ninjago58rus@gmail.com>
2022-09-06 12:25:52 +0300

    deploy

--- ToDoManager/ToDoManager/settings.py
+++ ToDoManager/ToDoManager/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': 'db',
-        'PORT':'5432',
+        'HOST': os.getenv("DB_HOST", '127.0.0.1'),
+        'PORT': os.getenv("DB_PORT", '5432'),
 
     }
 }
