70bbe8d6e986f4374fcfaa74af87c90f7fbc8834
evgeny <ninjago58rus@gmail.com>
2022-09-11 16:19:46 +0300

    auth

--- ToDoManager/core/urls.py
+++ ToDoManager/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from core.views import UserCreateView, login_user, UserProfileView, UserUpdatePwdView, logout_user
 
 urlpatterns = [
-    path('signup', UserCreateView.as_view(),name='signup'),
+    path('core/signup', UserCreateView.as_view(),name='signup'),
     path('login', login_user, name='login'),
     path('logout', logout_user, name='logout'),
     path('profile', UserProfileView.as_view(), name='profile'),
