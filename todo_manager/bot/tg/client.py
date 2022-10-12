1e146b6929b936e51694f8a4ed4899b65f5d8a71
evgeny <ninjago58rus@gmail.com>
2022-10-12 10:38:31 +0300

    lesson_38

--- todo_manager/bot/tg/client.py
+++ todo_manager/bot/tg/client.py
@@ -13,7 +13,8 @@ class TgClient:
         return f"https://api.telegram.org/bot{self.token}/{method}"
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
-        cl = requests.get(self.get_url(f'getUpdates?offset={offset}&timeout={timeout}'))
+        cl = requests.get(self.get_url('getUpdates'), json={'offset': offset, 'timeout': timeout})
+
         return GetUpdatesResponse(**cl.json())
 
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
